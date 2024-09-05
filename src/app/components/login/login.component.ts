e31fe4ac0127f420553c94f3c951b9483f12ba6a
bharaa <bharalfhani@gmail.com>
2024-09-06 01:56:15 +0700

    [IMPROVE] add layout for login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   credentials: any = { email: '', password: '' };
 
   constructor(private authService: AuthService, private router: Router) {}
@@ -19,12 +20,77 @@ export class LoginComponent {
   onSubmit() {
     this.authService.login(this.credentials).subscribe({
       next: () => {
-        // console.log(this.authService.currentUser);
         console.log("success login")
+        this.router.navigate(['']);
       },
       error: (err) => {
         console.log(err.error.responseMessage);
       },
+    });
+  }
+
+  private current: anime.AnimeInstance | null = null;
+
+  ngOnInit(): void {
+    this.setupAnimations();
+  }
+
+  ngOnDestroy(): void {
+    if (this.current) {
+      this.current.pause();
+    }
+  }
+
+  private setupAnimations(): void {
+    document.querySelector('#email')?.addEventListener('focus', () => {
+      if (this.current) this.current.pause();
+      this.current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+          value: 0,
+          duration: 700,
+          easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+          value: '240 1386',
+          duration: 700,
+          easing: 'easeOutQuart'
+        }
+      });
+    });
+
+    document.querySelector('#password')?.addEventListener('focus', () => {
+      if (this.current) this.current.pause();
+      this.current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+          value: -336,
+          duration: 700,
+          easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+          value: '240 1386',
+          duration: 700,
+          easing: 'easeOutQuart'
+        }
+      });
+    });
+
+    document.querySelector('#submit')?.addEventListener('focus', () => {
+      if (this.current) this.current.pause();
+      this.current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+          value: -730,
+          duration: 700,
+          easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+          value: '530 1386',
+          duration: 700,
+          easing: 'easeOutQuart'
+        }
+      });
     });
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jetbrainsmono: ["JetBrains", "sans-serif"],
+        teko: ["Teko", "sans-serif"]
+      },
+    },
   },
   plugins: [],
 };
