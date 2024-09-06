014d3444d0535569ba72a6c60d7af4401d5070f6
bharaa <bharalfhani@gmail.com>
2024-09-06 15:11:14 +0700

    [IMPROVE] add some image and text

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {
       fontFamily: {
         jetbrainsmono: ["JetBrains", "sans-serif"],
-        teko: ["Teko", "sans-serif"]
+        teko: ["Teko", "sans-serif"],
+      },
+      colors: {
+        "dark-grey": "#000000",
+        "light-blue": "#4F98CA",
+        "sport-green": "#50D890",
+        "rather-green": "#EFFFFB",
       },
     },
   },
